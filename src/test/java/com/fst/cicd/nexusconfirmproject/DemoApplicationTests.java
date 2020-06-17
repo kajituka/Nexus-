5c028a696fafa8f770d0b58d1a95d41ff6e38f2a
kajituka <kata13315@gmail.com>
2020-06-17 13:58:58 +0900

    Nexus確認用プロジェクト初コミット

---
+++ src/test/java/com/fst/cicd/nexusconfirmproject/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fst.cicd.nexusconfirmproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
